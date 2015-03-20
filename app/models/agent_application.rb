b2158c19cc805f7220534437e9cdf115da807586
Prelang Builder <builder@prelang.com>
2015-03-20 08:36:16 +0000

    Defined validation for attribute 'company_phone' on model 'AgentApplication'

--- app/models/agent_application.rb
+++ app/models/agent_application.rb
@@ -2,4 +2,5 @@ class AgentApplication < ActiveRecord::Base
   belongs_to :user
   validates_formatting_of :company_zip, using: :us_zip
   validates_formatting_of :payment_zip, using: :us_zip
+  validates_formatting_of :company_phone, using: :us_phone
 end
