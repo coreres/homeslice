7eb01dde8e0718fd8e7084b75008996509e591cb
Prelang Builder <builder@prelang.com>
2015-03-20 08:36:29 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'AgentApplication' to allow for create, update

--- app/admin/agent_application.rb
+++ app/admin/agent_application.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register AgentApplication do
+  permit_params :status, :company_name, :company_address, :company_city, :company_zip, :company_state, :check_payable_to, :payment_address, :payment_city, :payment_zip, :company_phone, :agent_cell_phone, :social_security, :company_fax, :license_number, :license_expiration, :eo_policy_number, :eo_expiration
 
 
   # See permitted parameters documentation:
