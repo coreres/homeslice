7f1676d80743df02e9f50a5ef57bed0db3e28dce
Prelang Builder <builder@prelang.com>
2015-03-20 08:36:23 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/agent_applications_controller.rb
+++ app/controllers/agent_applications_controller.rb
@@ -1,4 +1,8 @@
 class AgentApplicationsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_agent_application, only: [:show, :edit, :update, :destroy]
 
   # GET /agent_applications
